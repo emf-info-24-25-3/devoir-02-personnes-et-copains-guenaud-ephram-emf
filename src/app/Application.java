db470a41c7debf99b9bf38800f5e4883b3cb44c6
guenaud-ephram-emf <ephram.guenaud@studentfr.ch>
2025-02-05 09:10:44 +0100

    changement

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,16 @@
 package app;
 
-public class Application {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+import models.Personne;
 
+public class Application {
+    public static void main(String[] args) {
+        Personne personne1 = new Personne("Guenaud", "Ephram");
+        Personne personne2 = new Personne("Mao", "Thomas");
+        Personne personne3 = new Personne("Das Silva", "Alex");
+        boolean aReussis = personne1.ajouteCopain(personne3);
+        System.out.println(aReussis);
+        System.out.println(personne1.toString());
+        System.out.println(personne2.toString());
+        System.out.println(personne3.toString());
+    }
 }
--- src/models/Personne.java
+++ src/models/Personne.java
@@ -1,17 +1,77 @@
 package models;
 
 public class Personne {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+        private final static int MAX_COPAINS = 10;
+        private String nom;
+        private String prenom;
+        private Personne[] copains = new Personne[MAX_COPAINS];
 
+        public Personne(String nom, String prenom) {
+                this.nom = nom;
+                this.prenom = prenom;
+        }
+
+        public final String getNom() {
+                return this.nom;
+        }
+
+        public final String getPrenom() {
+                return this.prenom;
+        }
+
+        public String toString() {
+                String texte;
+                if (nombreDeCopain() == 0) {
+                        texte = getPrenom() + " " + getNom().toUpperCase() + " et ses copains ()";
+                } else {
+                        texte = getPrenom() + " " + getNom().toUpperCase() + " et ses copains (" + copainsComplet() + ")";
+                }
+                return texte;
+        }
+
+        public boolean ajouteCopain(Personne copain) {
+                boolean aReussis = false;
+                for (int i = 0; i < copains.length; i++) {
+                        if (copains[i] != null) {
+                                copains[i] = copain;
+                                aReussis = true;
+                                break;
+                        }
+                }
+                return aReussis;
+        }
+
+        public boolean retireCopain(Personne copain) {
+                boolean aReussis = false;
+                for (int i = 0; i < copains.length; i++) {
+                        if (copains[i] == copain) {
+                                copains[i] = null;
+                                aReussis = true;
+                                break;
+                        }
+                }
+                return aReussis;
+        }
+
+        public int nombreDeCopain() {
+                int compteur = 0;
+                for (int i = 0; i < copains.length; i++) {
+                        if (copains[i] != null) {
+                                compteur++;
+                        }
+                }
+                return compteur;
+        }
+
+        public Personne[] copainsComplet() {
+                Personne[] copainsComplet = new Personne[nombreDeCopain()];
+                int compteur = 0;
+                for (int i = 0; i < this.copains.length; i++) {
+                        if (copains[i] != null) {
+                                copainsComplet[compteur] = copains[i];
+                                compteur++;
+                        }
+                }
+                return copainsComplet;
+        }
 }
